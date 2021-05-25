5aa8f254384adf0fc50eb5c141d8601166b25eec
hvrain <kim49197@naver.com>
2021-05-26 01:55:07 +0900

    setState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <button type="button" onClick={() => {
-        fetch('/api/data')
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-        });
-      }}>get data</button>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    greet: ""
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button type="button" onClick={() => {
+          fetch('/api/data')
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            this.setState({greet:data.greeting});
+          });
+        }}>get data</button>
+        <div>{this.state.greet}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
